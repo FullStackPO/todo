a68e4b47fdd327f9c34aea0fb29ae4ec7ecde8c8
Harsh Arya <harsharya8946@gmail.com>
2026-04-13 21:38:55 +0530

    able to write tasks

---
+++ todo/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="flex p-5 bg-gradient-to-r from-blue-400 to-blue-800 align-center justify-center font-bold text-3xl">
+        <h1 className="text-black">Task Manager</h1>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
